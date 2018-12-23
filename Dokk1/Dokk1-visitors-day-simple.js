9c161a566a7691f20e4c03d9aed5d4e2bd6222dc
Henrik Korsgaard <korsgaard@cs.au.dk>
2018-12-23 22:38:26 +0100

    Update Dokk1-visitors-day-simple.js

--- Dokk1/Dokk1-visitors-day-simple.js
+++ Dokk1/Dokk1-visitors-day-simple.js
@@ -45,7 +45,7 @@ site context (href), visualisation type, user agent and a time stamp.
     var _position = window.location.href
     var _container, _body, _records, _today, _opens, _closes, _dataOffset;
     var _colors = {
-        open: "#88ccee",
+        open: "#66ccee",
         in: "#fe9929",
         out: "#fc4e2a",
         visitors: "#01665e",
@@ -417,7 +417,7 @@ site context (href), visualisation type, user agent and a time stamp.
 
         .${_namespace} a:last-child {
             position:absolute;
-            left:${svgWidth-200}px;
+            left:${svgWidth-190}px;
         }
 
         .${_namespace} a:active,  .${_namespace} a:hover,  .${_namespace} a:visited {
